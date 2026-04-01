59aa5da4997b4d90b086f637dd0e5f659d475126
yad <yad.hoshyar@gmail.com>
2026-04-02 02:21:02 +0300

    add custom redirect logic for authenticated users based on their roles

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Enums\UserRole;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,6 +17,16 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\EnsureRole::class,
         ]);
+
+        RedirectIfAuthenticated::redirectUsing(function ($request) {
+            $user = $request->user();
+
+            return match ($user->role) {
+                UserRole::Admin => route('admin.dashboard'),
+                UserRole::Teacher => route('teacher.dashboard'),
+                UserRole::Student => route('student.dashboard'),
+            };
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
