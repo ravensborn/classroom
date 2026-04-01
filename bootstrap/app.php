da81fb08f374453fafed72e6d468f0197d142441
yad <yad.hoshyar@gmail.com>
2026-04-02 01:28:45 +0300

    trust all proxies in middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies('*');
         $middleware->alias([
             'role' => \App\Http\Middleware\EnsureRole::class,
         ]);
